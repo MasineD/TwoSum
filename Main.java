7daeffa6171d48f1ebb53c039cbcef8f791dd116
MasineD <masinedonald@gmail.com>
2026-04-03 15:54:24 +0200

    Main(cold-code solution):This uploads the solution, written in java

---
+++ Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class  Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+        int [] data = new int[n];
+        System.out.print("Enter the elements: ");
+        for(int i = 0; i < n; i++){
+            data[i] = sc.nextInt();
+        }
+        System.out.print("Enter the target: ");
+        int target = sc.nextInt();
+        twoSum(data, target);
+    }
+
+    public static void twoSum(int [] data, int target){
+        if(data.length<2 || data.length> 10000){
+            System.out.println("Length of "+data.length+" is out of range");
+            return;
+        }
+        for(int i = 0; i<data.length-1;i++){
+            for(int j = i + 1;j < data.length;j++){
+                if((data[i] + data[j]) == target){
+                    System.out.println("["+i+","+j+"]");
+                    return;
+                }
+            }
+        }
+        System.out.println("No two elements sum up to the target.");
+    }
+}
